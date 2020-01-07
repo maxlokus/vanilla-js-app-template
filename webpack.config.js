55be830576d512819b870eb248d969770d9df36a
Max PM61Data <macklock@yandex.ru>
2020-01-07 15:49:00 +0300

    Style loaders added

--- webpack.config.js
+++ webpack.config.js
@@ -13,5 +13,21 @@ module.exports = {
         port: 3000
     },
 
-    plugins: [new HTMLPlugin({ template: "src/index.html" }), new CleanWebpackPlugin()]
+    plugins: [new HTMLPlugin({ template: "src/index.html" }), new CleanWebpackPlugin()],
+
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    "style-loader",
+                    // Translates CSS into CommonJS
+                    "css-loader",
+                    // Compiles Sass to CSS
+                    "sass-loader"
+                ]
+            }
+        ]
+    }
 };
